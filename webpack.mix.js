997b870fea2b3c49b292060d88f7990cc1900b5e
Niels Mokkentstorm <nielsmokkenstorm@gmail.com>
2017-11-14 21:48:30 +0100

    Frontend scaffolding
    Laravel ships with components for Passport / Vue things by default.
    Let's add some Tailwind to make stuff pretty in a non default way and we
    have a start for a nice setup.

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,9 @@ let mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+var tailwindcss = require('tailwindcss');
+
+mix.postCss('resources/assets/tailwind/app.css', 'public/css', [
+    tailwindcss('./tailwind.js'),
+]).js('resources/assets/js/app.js', 'public/js');
+
